cba47839e33206dd3fb215691a438035a5577ced
Petong - KOALA <78460477+koala45601@users.noreply.github.com>
2021-03-10 12:57:39 +0700

    update date
    update date line 19,27,28

--- Time/Time.py
+++ Time/Time.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import time 
 import datetime as date
+from tkinter import tkinter
 
 
 windown = tk.Tk()
@@ -10,10 +11,12 @@ def Time_result():
     Time_H = time.strftime('%H')
     Time_M = time.strftime('%M')
     Time_S = time.strftime('%S')
+    Time_D = time.strftime('%a')
     date_1=date.datetime.now()
     #cl_0 = print(f'{Time_H}:{Time_M}:{Time_S}')
     L_1.config(text=f'{Time_H}:{Time_M}:{Time_S} ',font=50)
     L_2.config(text=date_1,font=50)
+    L_3.config(text=f'Date : Time_D')
     L_1.after(1000,Time_result)
 
 
@@ -21,6 +24,8 @@ L_1 = tk.Label(windown,text="Time NOW : ")
 L_1.pack()
 L_2 = tk.Label(windown)
 L_2.pack()
+L_3 = Label(windown)
+L_3.pack(side = LEFT,anchor=NW)
 Time_result()
 
 
