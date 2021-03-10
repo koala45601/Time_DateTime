ef3e14e195364abe694c4a206060ec97090bd5bd
Petong - KOALA <78460477+koala45601@users.noreply.github.com>
2021-03-10 16:05:46 +0700

    update time date and disable tab tital

--- Time/Time.py
+++ Time/Time.py
@@ -10,10 +10,12 @@ def Time_result():
     Time_H = time.strftime('%H')
     Time_M = time.strftime('%M')
     Time_S = time.strftime('%S')
+    Time_D= time.strftime('%a')
     date_1=date.datetime.now()
     #cl_0 = print(f'{Time_H}:{Time_M}:{Time_S}')
     L_1.config(text=f'{Time_H}:{Time_M}:{Time_S} ',font=50)
     L_2.config(text=date_1,font=50)
+    L_3.config(text=f'Date : {Time_D}',font=50)
     L_1.after(1000,Time_result)
 
 
@@ -21,8 +23,10 @@ L_1 = tk.Label(windown,text="Time NOW : ")
 L_1.pack()
 L_2 = tk.Label(windown)
 L_2.pack()
+L_3 = tk.Label(windown,)
+L_3.pack()
 Time_result()
 
-
-windown.geometry('250x50')
+windown.overrideredirect(1)
+windown.geometry('350x80')
 windown.mainloop()
