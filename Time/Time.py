fe1b179a0dd0f28eb7e3300d923bc0afaf7d7cd2
Petong - KOALA <78460477+koala45601@users.noreply.github.com>
2021-03-17 11:28:50 +0700

    initial commit

--- Time/Time.py
+++ Time/Time.py
@@ -5,17 +5,19 @@ import datetime as date
 
 windown = tk.Tk()
 windown.title('Clock')
+windown.configure(background='LAWNGREEN')
 
 def Time_result():
+
     Time_H = time.strftime('%H')
     Time_M = time.strftime('%M')
     Time_S = time.strftime('%S')
     Time_D= time.strftime('%a')
     date_1=date.datetime.now()
     #cl_0 = print(f'{Time_H}:{Time_M}:{Time_S}')
-    L_1.config(text=f'{Time_H}:{Time_M}:{Time_S} ',font=50)
-    L_2.config(text=date_1,font=50)
-    L_3.config(text=f'Date : {Time_D}',font=50)
+    L_1.config(text=f'{Time_H}:{Time_M}:{Time_S} ',font=('Open 24 Display St',70),bg='LAWNGREEN',fg='#00008C')
+    #L_2.config(text=date_1,font=50)
+    #L_3.config(text=f'Date : {Time_D}',font=50)
     L_1.after(1000,Time_result)
 
 
@@ -29,5 +31,5 @@ Time_result()
 
 windown.overrideredirect(1)
 #350x80 =โซลูชันหน้าจอ || 1600+960 คือวางแอพไว้ตรงไหนของหน้าจอ จริงๆใช้แค่ 350x80 ก็พอแล้วแต่หน้าจอจะ Random เกิดแต่ล่ะรอบที่รัน
-windown.geometry('350x80+1600+960')
+windown.geometry('350x90')
 windown.mainloop()
